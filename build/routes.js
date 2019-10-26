5d4374dfe12ff69b31787aa03ac7642a5105e752
brennowilliam <brennowilliam@gmail.com>
2019-10-26 20:30:46 -0300

    remove build and .vscode directories

--- build/routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// Express
-const express_1 = require("express");
-const routes = express_1.Router();
-routes.get("/", (req, res) => {
-    return res.json({ message: "Hello, world" });
-});
-exports.default = routes;
